c1bfbda8a1511fe57f7bb3d695dad858bd6d62b5
mitshel <mitshel@mail.ru>
2014-01-11 15:51:49 +0400

    Проведено несколько тестов производительности Скрипт Speed показывает приблизительную скорость добавления книг (на этапе работы sopds-scan.py)

--- py/fb2parse.py
+++ py/fb2parse.py
@@ -4,13 +4,12 @@
 import sopdsparse
 import base64
 
-  
 f=open('book1.fb2','rb')
-parser=sopdsparse.fb2parser(1)
+parser=sopdsparse.fb2parser(True)
 parser.parse(f)
 #print(parser.author_first.getvalue(), parser.author_last.getvalue(), parser.genre.getvalue(), parser.lang.getvalue(), parser.book_title.getvalue())
 #i=0
-#print(parser.cover_image.cover_data)
+print(parser.cover_image.cover_data)
 #jpg=open('image.jpg','wb')
 #sstr=parser.cover_image.cover_data.strip("' ")
 #dstr=base64.b64decode(sstr)
--- py/zipf.py
+++ py/zipf.py
@@ -1046,8 +1046,8 @@ class ZipFile:
 
     def printdir(self, file=None):
         """Print a table of contents for the zip file."""
-        print("%-46s %19s %12s" % ("File Name", "Modified    ", "Size"),
-              file=file)
+        print("%-46s %19s %12s" % ("File Name", "Modified    ", "Size"), 
+                  file=file)
         for zinfo in self.filelist:
             date = "%d-%02d-%02d %02d:%02d:%02d" % zinfo.date_time[:6]
             print("%-46s %s %12d" % (zinfo.filename, date, zinfo.file_size),
