fdb0ba5dd8c010c75ddf23986794e1d3dea8dbbf
Dmitry Shelepnev <mitshel@mail.ru>
2016-10-09 22:07:35 +0300

    Some changes

--- sopds_web_backend/urls.py
+++ sopds_web_backend/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import url
 from sopds_web_backend import views
 
 urlpatterns = [
-    url(r'^search/books/(?P<searchtype>[bmasgued])/(?P<searchterms>.+)/(?P<page>\d+)/',views.SearchBooksView, name='searchbooks'),
-    url(r'^search/books/(?P<searchtype>[bmasgued])/(?P<searchterms>.+)/',views.SearchBooksView, name='searchbooks'),    
-    url(r'^search/books/(?P<searchtype>as)/(?P<searchterms>.+)/(?P<searchterms0>.+)/(?P<page>\d+)/',views.SearchBooksView, name='searchbooks'),
-    url(r'^search/books/(?P<searchtype>as)/(?P<searchterms>.+)/(?P<searchterms0>.+)/',views.SearchBooksView, name='searchbooks'),
-    url(r'^search/books/(?P<searchtype>as)/(?P<searchterms>.+)/',views.SelectSeriesView, name='searchbooks'), 
-    url(r'^search/books/u/0/',views.SearchBooksView, name='bookshelf'),
+    url(r'^search/books/',views.SearchBooksView, name='searchbooks'),
+#    url(r'^search/books/(?P<searchtype>[bmasgued])/(?P<searchterms>.+)/(?P<page>\d+)/',views.SearchBooksView, name='searchbooks'),
+#    url(r'^search/books/(?P<searchtype>[bmasgued])/(?P<searchterms>.+)/',views.SearchBooksView, name='searchbooks'),    
+#    url(r'^search/books/(?P<searchtype>as)/(?P<searchterms>.+)/(?P<searchterms0>.+)/(?P<page>\d+)/',views.SearchBooksView, name='searchbooks'),
+#    url(r'^search/books/(?P<searchtype>as)/(?P<searchterms>.+)/(?P<searchterms0>.+)/',views.SearchBooksView, name='searchbooks'),
+#    url(r'^search/books/(?P<searchtype>as)/(?P<searchterms>.+)/',views.SelectSeriesView, name='searchbooks'), 
+#    url(r'^search/books/u/0/',views.SearchBooksView, name='bookshelf'),
                  
     url(r'^search/authors/(?P<searchtype>[bme])/(?P<searchterms>.+)/(?P<page>\d+)/',views.SearchAuthorsViews, name='searchauthors'),
     url(r'^search/authors/(?P<searchtype>[bme])/(?P<searchterms>.+)/',views.SearchAuthorsViews, name='searchauthors'),       
--- sopds_web_backend/views.py
+++ sopds_web_backend/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, render_to_response, redirect, Http404
+from django.template import Context, RequestContext
+from django.template.context_processors import csrf
+
+from opds_catalog.models import Book
+
 
 # Create your views here.
 
-def SearchBooksView(request, searchtype=None, searchterms=None, searchterms0=None, page=None):
-    return
+def SearchBooksView(request):
+    #Read searchtype, searchterms, searchterms0, page from form
+    args = RequestContext(request)
+    args.update(csrf(request))
+    if request.POST:
+        searchtype = request.POST.get('searchtype', 'm')
+        searchterms = request.POST.get('searchterms', '')
+        #searchterms0 = int(request.POST.get('searchterms0', ''))
+        #page = int(request.POST.get('page', '0'))
+        
+        books = Book.objects.extra(where=["upper(title) like %s"], params=["%%%s%%"%searchterms.upper()])
+        
+    args['searchterms']=searchterms;
+    args['searchtype']=searchtype;
+    args['books']=books
+       
+    return render_to_response('sopds_body.html', args)
 
 def SelectSeriesView(request, searchtype=None, searchterms=None, page=None):
     return
