3ce5f1ec8780b5039c4596d251dbd5ba88ea1c2d
Dmitry Shelepnev <mitshel@mail.ru>
2016-11-09 17:59:05 +0300

    New translation symbol in utils.translit

--- opds_catalog/utils.py
+++ opds_catalog/utils.py
@@ -7,7 +7,15 @@ def translit(s):
    assert s is not str, "Error: argument MUST be string"
 
    table1 = str.maketrans("абвгдеёзийклмнопрстуфхъыьэАБВГДЕЁЗИЙКЛМНОПРСТУФХЪЫЬЭ",  "abvgdeezijklmnoprstufh'y'eABVGDEEZIJKLMNOPRSTUFH'Y'E")
-   table2 = {'ж':'zh','ц':'ts','ч':'ch','ш':'sh','щ':'sch','ю':'ju','я':'ja',  'Ж':'Zh','Ц':'Ts','Ч':'Ch','Ш':'Sh','Щ':'Sch','Ю':'Ju','Я':'Ja', '«':'\'', '»':'\'','"':'\'','\n':' '}
+   table2 = {'ж':'zh','ц':'ts','ч':'ch','ш':'sh','щ':'sch','ю':'ju','я':'ja',  'Ж':'Zh','Ц':'Ts','Ч':'Ch','Ш':'Sh','Щ':'Sch','Ю':'Ju','Я':'Ja', 
+             '«':'', '»':'','"':'','\n':'_',' ':'_',"'":"",':':'_','№':'N'}
    for k in table2.keys():
        s = s.replace(k,table2[k])
-   return s.translate(table1)
+   return s.translate(table1)
+def badsym(s):
+    """Replace special web-symbols"""
+    result = s
+    table = {' ':'_', "'":"", '"':'', '№':'N'}
+    for k in table.keys():
+        result = result.replace(k,table[k])
+    return result;
