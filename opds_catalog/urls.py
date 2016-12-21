50314ec424b13d348a0d014030d58d3f9de63d63
Dmitry Shelepnev <mitshel@mail.ru>
2016-12-21 19:28:41 +0300

    Fix issue #22. Clear hardcoded yrls in sopds_books_html

--- opds_catalog/urls.py
+++ opds_catalog/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
     url(r'^convert/(?P<book_id>[0-9]+)/(?P<convert_type>.+)/$',dl.ConvertFB2, name='convert'),    
     url(r'^download/(?P<book_id>[0-9]+)/(?P<zip_flag>[0-1])/$',dl.Download, name='download'),
     url(r'^cover/(?P<book_id>[0-9]+)/$',dl.Cover, name='cover'),    
+    url(r'^cover/$',dl.Cover, name='covertmpl'),  
         
     url(r'^',feeds.MainFeed(), name='main'),
 ]
