7d01f2377e7d52494caeb4b5b4a6137105f006cf
Dmitry.Shelepnev <mitshel@mail.ru>
2018-08-30 21:04:20 +0300

    Django-constance package integrate in project from github.

---
+++ constance/__init__.py
@@ -0,0 +1,14 @@
+from django.utils.functional import LazyObject
+
+__version__ = '2.2.0'
+
+default_app_config = 'constance.apps.ConstanceConfig'
+
+
+class LazyConfig(LazyObject):
+    def _setup(self):
+        from .base import Config
+        self._wrapped = Config()
+
+
+config = LazyConfig()
---
+++ constance/static/admin/js/constance.js
@@ -0,0 +1,26 @@
+(function($) {
+    'use strict';
+
+    $(function() {
+
+        $('#content-main').on('click', '.reset-link', function(e) {
+            e.preventDefault();
+
+            var field = $('#' + this.dataset.fieldId);
+            var fieldType = this.dataset.fieldType;
+
+            if (fieldType === 'checkbox') {
+                field.prop('checked', this.dataset.default === 'true');
+            } else if (fieldType === 'date') {
+                var defaultDate = new Date(this.dataset.default * 1000);
+                $('#' + this.dataset.fieldId).val(defaultDate.strftime(get_format('DATE_INPUT_FORMATS')[0]));}
+            } else if (fieldType === 'datetime') {
+                var defaultDate = new Date(this.dataset.default * 1000);
+                $('#' + this.dataset.fieldId + '_0').val(defaultDate.strftime(get_format('DATE_INPUT_FORMATS')[0]));
+                $('#' + this.dataset.fieldId + '_1').val(defaultDate.strftime(get_format('TIME_INPUT_FORMATS')[0]));
+            } else {
+                field.val(this.dataset.default);
+            }
+        });
+    });
+})(django.jQuery);
