91f5ab6ffce77a73ce793af9484ece7c37dc3ebd
Igor Murzich <imurzich@gmail.com>
2023-12-19 21:15:05 +0300

    В Pillow 10 ANTIALIAS заменён на LANCZOS

--- opds_catalog/dl.py
+++ opds_catalog/dl.py
@@ -254,7 +254,7 @@ def Cover(request, book_id, thumbnail=False):
         response["Content-Type"] = 'image/jpeg'
         if thumbnail:
             thumb = Image.open(io.BytesIO(image)).convert('RGB')
-            thumb.thumbnail((settings.THUMB_SIZE, settings.THUMB_SIZE), Image.ANTIALIAS)
+            thumb.thumbnail((settings.THUMB_SIZE, settings.THUMB_SIZE), Image.LANCZOS)
             tfile = io.BytesIO()
             thumb.save(tfile, 'JPEG')
             image = tfile.getvalue()
@@ -308,7 +308,7 @@ def Cover0(request, book_id, thumbnail = False):
                 if thumbnail:
                     response["Content-Type"] = 'image/jpeg'
                     thumb = Image.open(io.BytesIO(dstr)).convert('RGB')
-                    thumb.thumbnail((settings.THUMB_SIZE, settings.THUMB_SIZE), Image.ANTIALIAS)
+                    thumb.thumbnail((settings.THUMB_SIZE, settings.THUMB_SIZE), Image.LANCZOS)
                     tfile = io.BytesIO()
                     thumb.save(tfile, 'JPEG')
                     dstr = tfile.getvalue()
